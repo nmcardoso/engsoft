2ced1e0d02581c73ad611d5dbca488c3f11e4c5b
Natanael <natanael_magalhaes@msn.com>
2021-04-29 01:38:56 -0300

    server deploy

--- server/deploy.sh
+++ server/deploy.sh
@@ -2,8 +2,7 @@
 git clone https://github.com/nmcardoso/engsoft.git /tmp/deploy
 
 # move files
-rm -rf src
-cp -r /tmp/deploy/server .
+cp -r /tmp/deploy/server/* .
 rm -rf /tmp/deploy
 
 # restart editor
