ddeae60e2cfec4dcbc17539e3e70073a2e843d7e
Natanael <natanael_magalhaes@msn.com>
2021-05-13 05:07:39 -0300

    initial express app

---
+++ server/deploy.sh
@@ -0,0 +1,8 @@
+# clone repo
+git clone https://github.com/nmcardoso/engsoft.git /tmp/deploy
+
+# move files
+mv /tmp/deploy/server/* .
+
+# restart editor
+restart
---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const cmd = require('node-cmd')
+
+const app = express()
+app.use(express.json())
+
+// Deploy Route
+app.post('/deploy', (req, res) => {
+  if (req.header.deploy_key !== process.env.deploy_key) {
+    return res.status(401)
+  }
+
+  cmd.get('bash deploy.sh', (err, data) => {
+    if (err) return console.log(err)
+    console.log(data)
+    return res.status(200).send(data)
+  })
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Your app is listening on port ${process.env.PORT}`)
+})
