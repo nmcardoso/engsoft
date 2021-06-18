51f2180d6b990e92414fab48db9edc51e22ebd79
Natanael <natanael_magalhaes@msn.com>
2021-06-18 18:26:36 -0300

    group routes

---
+++ api/routes/auth.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const jwt = require('jsonwebtoken')
+const Login = require('../models/Login')
+
+const router = express.Router()
+
+router.post('/login', async (req, res) => {
+  const username = req.body.username
+  const password = req.body.password
+
+  const login = await Login.findOne({
+    where: {
+      username,
+      password
+    }
+  })
+
+  if (login == null) {
+    return res.status(401).json({
+      success: false,
+      message: 'Credenciais inválidas.'
+    })
+  }
+
+  const user = { name: username }
+
+  const accessToken = jwt.sign(user, process.env.JWT_ACCESS_SECRET)
+  res.json({
+    success: true,
+    accessToken: accessToken
+  })
+})
+
+router.post('/register', async (req, res) => {
+  const login = await Login.findOne({
+    where: {
+      username: req.body.username
+    }
+  })
+
+  if (login != null) {
+    return res.json({
+      success: false,
+      message: 'Nome de usuário já existe.'
+    })
+  }
+
+  const newUser = await Login.create({
+    username: req.body.username,
+    password: req.body.password
+  })
+
+  res.json({
+    success: true,
+    message: 'Usuário criado com sucesso.'
+  })
+})
+
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -5,10 +5,8 @@ if (process.env.NODE_ENV === 'dev') {
 }
 
 const express = require('express')
-const { Pool } = require('pg')
-const jwt = require('jsonwebtoken')
 const db = require('./config/database')
-const Login = require('./models/Login')
+const authRouter = require('./routes/auth')
 
 db.authenticate()
   .then(() => console.log('Database connected'))
@@ -16,19 +14,7 @@ db.authenticate()
 
 const app = express()
 app.use(express.json())
-
-function authenticateToken(req, res, next) {
-  const authHeader = req.headers['authorization']
-  const token = authHeader && authHeader.split(' ')[1]
-
-  if (token == null) return res.sendStatus(401)
-
-  jwt.verify(token, process.env.JWT_ACCESS_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403)
-    req.user = user
-    next()
-  })
-}
+app.use('/api/auth', authRouter)
 
 app.get('/api', (req, res) => {
   res.send('Hello World!')
@@ -38,66 +24,6 @@ app.get("/api/health", (req, res) => {
   res.sendStatus(200)
   return
 })
-
-
-app.post('/api/login', async (req, res) => {
-  const username = req.body.username
-  const password = req.body.password
-
-  const login = await Login.findOne({
-    where: {
-      username,
-      password
-    }
-  })
-
-  if (login == null) {
-    return res.status(401).json({
-      success: false,
-      message: 'Credenciais inválidas.'
-    })
-  }
-
-  const user = { name: username }
-
-  const accessToken = jwt.sign(user, process.env.JWT_ACCESS_SECRET)
-  res.json({
-    success: true,
-    accessToken: accessToken
-  })
-})
-
-
-app.post('/api/register', async (req, res) => {
-  const login = await Login.findOne({
-    where: {
-      username: req.body.username
-    }
-  })
-
-  if (login != null) {
-    return res.json({
-      success: false,
-      message: 'Nome de usuário já existe.'
-    })
-  }
-
-  const newUser = await Login.create({
-    username: req.body.username,
-    password: req.body.password
-  })
-
-  res.json({
-    success: true,
-    message: 'Usuário criado com sucesso.'
-  })
-})
-
-
-app.get('/api/posts', authenticateToken, (req, res) => {
-  res.json({ auth: 'ok' })
-})
-
 
 app.listen(8080, () => {
   console.log('Server running on port 8080.')
