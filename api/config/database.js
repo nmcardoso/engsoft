6802884f1cee42e2585f952f2aa6c6de61680843
Natanael <natanael_magalhaes@msn.com>
2021-06-18 12:20:23 -0300

    log env

--- api/config/database.js
+++ api/config/database.js
@@ -1,5 +1,7 @@
 const Sequelize = require('sequelize')
 
+console.log(process.env.PG_CONNECTION)
+
 module.exports = new Sequelize(process.env.PG_CONNECTION, {
   define: {
     freezeTableName: true
