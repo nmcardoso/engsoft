1aa13e9218813c9d0cfeae55c3dca96ec2a13653
Natanael <natanael_magalhaes@msn.com>
2021-06-18 12:44:38 -0300

    remove prints

--- api/config/database.js
+++ api/config/database.js
@@ -1,7 +1,5 @@
 const Sequelize = require('sequelize')
 
-console.log(process.env.PG_CONNECTION)
-
 module.exports = new Sequelize(process.env.PG_CONNECTION, {
   define: {
     freezeTableName: true
