07b76f0558afd2479c7bb3aaec6dccd4071b2b8c
Natanael <natanael_magalhaes@msn.com>
2021-06-21 18:21:14 -0300

    implement router

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,24 @@
-import Form from './Form'
-import Navbar from './Navbar'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
+
+import Login from './Login'
+import Register from './Register'
 
 function App() {
   return (
-    <div>
-      <header>
-        <Navbar />
-      </header>
-
-      <div className="container mt-3">
-        <Form />
-      </div>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
 
