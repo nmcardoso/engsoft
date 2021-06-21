616e3be6a8afb2938ace8daf4b559fca9a28a9c2
Natanael <natanael_magalhaes@msn.com>
2021-06-21 18:20:04 -0300

    api interface

---
+++ client/src/API.js
@@ -0,0 +1,30 @@
+import axios from 'axios'
+
+class API {
+  constructor() {
+    this.url = process.env.NODE_ENV === 'production' ?
+      'https://engsoft-production-nmcardoso.cloud.okteto.net/' : 'http://localhost:8080'
+  }
+
+  getUrl(route) {
+    return this.url + route
+  }
+
+  login({ username, password }) {
+    return axios({
+      url: this.getUrl('/auth/login'),
+      method: 'post',
+      data: { username, password }
+    })
+  }
+
+  register({ username, password }) {
+    return axios({
+      url: this.getUrl('/auth/register'),
+      method: 'post',
+      data: { username, password }
+    })
+  }
+}
+
+export default API
