e27ebd07b13d51f521b65d1f4f1e33e6a81768aa
Natanael <natanael_magalhaes@msn.com>
2021-06-25 02:02:02 -0300

    populate db script

---
+++ api/utils/populate_db.js
@@ -0,0 +1,57 @@
+const { exec } = require('child_process')
+
+const load_env = require('./load_env')
+load_env()
+const db = require('../config/database')
+
+async function populate() {
+  // load pg connection password
+  process.env.PGPASSWORD = process.env.PG_PASS
+
+  // load all models
+  const models = fs.readdirSync(path.resolve(__dirname, '../models'))
+  models.forEach(model => {
+    require('../models/' + model)
+  })
+
+  const command = [
+    'pg_restore',
+    `-U ${process.env.PG_USER}`,
+    '-w',
+    `-h ${process.env.PG_HOST}`,
+    `-p ${process.env.PG_PORT}`,
+    `-d ${process.env.PG_DB}`,
+    '-c',
+    '-1',
+    '--no-owner',
+    'sql/database-data.psql'
+  ]
+  exec(command.join(' '), async (err, stdout, stderr) => {
+    if (err) {
+      console.log(err)
+      return
+    }
+
+    if (stderr) {
+      console.log(stderr)
+      return
+    }
+
+    console.log('\n')
+    console.log(models)
+    console.log('\n')
+
+    const tables = await db.getQueryInterface().showAllTables()
+
+    for (const table of tables) {
+      await db.query(`SELECT setval('${table}_id_seq', (SELECT MAX(id) FROM "${table}"))`)
+    }
+
+    console.log(stdout)
+    console.log('>> Banco de dados populado com sucesso')
+  })
+}
+
+populate()
+
+
