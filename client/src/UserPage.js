bce4d77462827a764a9aa307c773cf557880b58c
Natanael <natanael_magalhaes@msn.com>
2021-06-25 02:02:02 -0300

    implement sync

--- client/src/UserPage.js
+++ client/src/UserPage.js
@@ -1,56 +1,131 @@
 import './UserPage.css'
 import { Link } from 'react-router-dom'
 import { useAuth } from './AuthContext'
-import { useReducer, useState } from 'react'
+import { useEffect, useReducer, useState } from 'react'
+import API from './services/API'
 
-function UserPage () {
-    const { user } = useAuth()
-    
-    return (
-        <div className="pageContainer">
-            <div className="limiter">
-                <nav className="navbar navbar-light bg-light rounded-top mx-5 mt-3">
-                    <div className="container-fluid">
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                            <span> Menu</span>
-                        </button>
-                        <span className=''>Página do usuário</span>
-                    </div>
-                </nav>
+function UserPage() {
+  const { user, logout } = useAuth()
+  const [unsynced, setUnsynced] = useState(undefined)
+  const [loading, isLoading] = useState(false)
 
-                <div className="collapse" id="navbarToggleExternalContent">
-                    <div className="bg-light pb-3 pt-1 mx-5 px-4 d-flex justify-content-center">
+  useEffect(async () => {
+    const api = new API()
+    try {
+      const r = await api.getUnsyncedForms(user.id_unidade_saude)
+      setUnsynced(r?.data?.count)
+    } catch (err) {
+      console.log(err)
+    }
+  }, [])
 
-                        {/* colocar as coisas que vao dentro da navbar aqui */}
+  const handleSync = async () => {
+    const api = new API()
+    isLoading(true)
+    try {
+      const r = await api.syncForms(user.id_unidade_saude)
+      if (r.data.success) {
+        isLoading(false)
+        setUnsynced(0)
+      } else {
+        isLoading(false)
+        console.log(r.data)
+        alert('Ocorreu um erro durante a sincronização com o Vacivida')
+      }
+    } catch (err) {
+      console.log(err)
+      isLoading(false)
+    }
+  }
 
-                        <div className="ContainerButton d-inline align-middle">
-                            <Link
-                                to='/dashboard'>
-                                <button className='btn btn-light fw-bold py-3 px-5 border border-success mx-2'>Formulário ✏️️</button>
-                            </Link>
-                            <Link
-                                to='/powerbi'>
-                                <button className='btn btn-light fw-bold py-3 px-5 border border-success mx-2'>Análise 📊</button>
-                            </Link>
-                            <Link
-                                to='/#'>
-                                <button className='btn btn-light fw-bold py-3 px-5 border border-success mx-2'>Feedack 🔁</button>
-                            </Link>
-                        </div>
-                    </div>
-                </div>
-                <div className="userInfo">
-                    <h4>{user.nome}</h4>
-                    <h6>Unidiade de saúde: {user.id_unidade_saude}</h6>
-                    <hr />
-                    <a href='' className='userActions'>Deslogar</a>
-                    <p />
-                    <a href='' className='userActions'>Trocar senha</a>
-                </div>
+  return (
+    <div className="pageContainer">
+      <div className="limiter">
+        <nav className="navbar navbar-light bg-light rounded-top mx-5 mt-3">
+          <div className="container-fluid">
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+              <span> Menu</span>
+            </button>
+            <span className=''>Página do usuário</span>
+          </div>
+        </nav>
+
+        <div className="collapse" id="navbarToggleExternalContent">
+          <div className="bg-light pb-3 pt-1 mx-5 px-4 d-flex justify-content-center">
+
+            {/* colocar as coisas que vao dentro da navbar aqui */}
+
+            <div className="ContainerButton d-inline align-middle">
+              <Link
+                to='/dashboard'>
+                <button className='btn btn-light fw-bold py-3 px-5 border border-success mx-2'>Formulário ✏️️</button>
+              </Link>
+              <Link
+                to='/powerbi'>
+                <button className='btn btn-light fw-bold py-3 px-5 border border-success mx-2'>Análise 📊</button>
+              </Link>
+              <Link
+                to='/#'>
+                <button className='btn btn-light fw-bold py-3 px-5 border border-success mx-2'>Feedack 🔁</button>
+              </Link>
             </div>
+          </div>
         </div>
-    )
+        <div className="userInfo">
+          <h4>{user.nome}</h4>
+          <h6>Unidiade de saúde: {user?.unidade_saude?.nome}</h6>
+          <h6>Endereço: {user?.unidade_saude?.endereco}</h6>
+
+          <hr />
+
+          {unsynced === undefined ? (
+            <span className="text-muted">
+              Carregando dados de sincronização dos formulários...
+            </span>
+          ) : (
+            <>
+              {unsynced > 0 ? (
+                <>
+                  <span className="me-3">
+                    Há <b>{unsynced}</b> formulário(s) a ser(em) sincronizado(s).
+                  </span>
+                  {loading ? (
+                    <button
+                      className="btn btn-success fw-bold py-2 px-3 mx-3"
+                      type="submit"
+                      disabled>
+                      <span
+                        className="spinner-border spinner-border-sm"
+                        role="status"
+                        aria-hidden="true" />
+                      &nbsp;&nbsp;&nbsp;Autenticando...
+                    </button>
+                  ) : (
+                    <button
+                      className="btn btn-success"
+                      onClick={handleSync}>
+                      Sincronizar agora
+                    </button>
+                  )}
+                </>
+              ) : (
+                <span className="text-success">Todos os formulários estão sincronizados</span>
+              )}
+            </>
+          )}
+
+          <hr />
+
+          <button
+            className="btn btn-danger"
+            onClick={() => logout()}>
+            Deslogar
+          </button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default UserPage
