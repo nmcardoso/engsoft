7d04fbbfc88a9128538c253fd0e7c1a227500844
Natanael <natanael_magalhaes@msn.com>
2021-07-01 17:23:15 -0300

    sync_db command

---
+++ api/utils/sync_db.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+const path = require('path')
+
+const db = require('../config/database')
+
+
+const models = fs.readdirSync(path.resolve(__dirname, '../models'))
+models.forEach(model => {
+  require('../models/' + model)
+})
+
+async function sync() {
+  await db.sync({ force: true })
+}
+
+sync()
