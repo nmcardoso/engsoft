f6f25db5c92d916ba3b382d3c0743ae485750d6b
Natanael <natanael_magalhaes@msn.com>
2021-07-10 14:34:21 -0300

    melhoramento da implementação da rota da api `unidade_saude`
    Adiçaõ do parâmetro `campos` para redução do payload

--- api/routes/unidade_saude.js
+++ api/routes/unidade_saude.js
@@ -4,9 +4,29 @@ const UnidadeSaude = require('../models/UnidadeSaude')
 
 const router = express.Router()
 
-router.get('/all', async (req, res) => {
-  const data = await UnidadeSaude.findAll()
-  console.log(data)
+router.get('/', async (req, res) => {
+  let data = []
+  if (req.query.campos) {
+    const campos = req.query.campos.split(',')
+    try {
+      data = await UnidadeSaude.findAll({
+        attributes: campos
+      })
+    } catch (err) {
+      let message = ''
+      if (err?.original?.routine === 'errorMissingColumn') {
+        message = 'pelo menos um dos campos especificados na requisição está incorreto'
+      }
+      return res.status(500).json({ success: false, message })
+    }
+  } else {
+    try {
+      data = await UnidadeSaude.findAll()
+    } catch (err) {
+      return res.status(500).json({ success: false })
+    }
+  }
+
   res.send(data)
 })
 
