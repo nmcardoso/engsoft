20f1d772e95daeb16ecdf26962fb889a9280c1b1
Natanael <natanael_magalhaes@msn.com>
2021-07-15 16:55:40 -0300

    auth context

---
+++ client/src/AuthContext.js
@@ -0,0 +1,103 @@
+import { createContext, useContext, useEffect, useState } from 'react'
+import API from './API'
+
+const AuthContext = createContext()
+
+export function AuthProvider({ children }) {
+  const [state, setState] = useState({
+    user: null,
+    session: null
+  })
+  const api = new API()
+
+  useEffect(() => {
+    const auth = JSON.parse(localStorage.getItem('auth'))
+    if (auth) {
+      api.setAuthToken(auth.session.token)
+      setState(auth)
+    }
+  }, [])
+
+  async function login({ username, password }) {
+    let message, success
+
+    try {
+      const resp = await api.login({ username, password })
+
+      localStorage.setItem('auth', JSON.stringify({
+        user: resp.data.userInfo,
+        session: { token: resp.data.token, logged: true }
+      }))
+
+      api.setAuthToken(resp.data.token)
+
+      message = resp.data.message
+      success = resp.data.success
+
+      setState({
+        user: resp.data.userInfo,
+        session: { token: resp.data.token, logged: true }
+      })
+    } catch (err) {
+      message = err.response.data.message
+      success = err.response.data.success
+
+      setState({
+        user: null,
+        session: null
+      })
+    }
+
+    return { success, message }
+  }
+
+  function logout() {
+    localStorage.removeItem('auth')
+
+    api.removeAuthToken()
+
+    setState({
+      user: null,
+      session: null,
+      status: null
+    })
+  }
+
+  // function setUser(user) {
+  //   const userStr = JSON.stringify(user)
+  //   localStorage.setItem('user', userStr)
+
+  //   const api = new API()
+  //   api.setAuthToken(user.token)
+
+  //   _setUser(user)
+  // }
+
+  // function removeUser() {
+  //   localStorage.removeItem('user')
+
+  //   const api = new API()
+  //   api.removeAuthToken()
+
+  //   _setUser(null)
+  // }
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user: state.user,
+        session: state.session,
+        status: state.status,
+        login,
+        logout
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export function useAuth() {
+  const context = useContext(AuthContext)
+  return context
+}
