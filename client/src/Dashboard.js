62cf71df5bc6f67ac3d1cae4a82c71d4949874b6
Natanael <natanael_magalhaes@msn.com>
2021-07-15 16:58:23 -0300

    remove implementação da renderização condicional anterior e carrega o objeto usuário pelo hook `useAuth`

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,24 +1,8 @@
-import { useEffect, useState } from 'react'
-import { useHistory } from 'react-router-dom'
-
-import API from './API'
+import { useAuth } from './AuthContext'
 import './Dashboard.css'
 
 function Dashboard() {
-  const [firstLoad, isFirstLoad] = useState(true)
-  const [user, setUser] = useState({})
-  const history = useHistory
-
-  useEffect(() => {
-    const api = new API()
-    console.log(localStorage.getItem('token'))
-    api.validateToken(localStorage.getItem('token')).then(resp => {
-      setUser({ name: resp.data.name })
-      isFirstLoad(false)
-    }).catch(() => {
-      history.push('/login')
-    })
-  }, [firstLoad])
+  const { user } = useAuth()
 
   return (
     <div className="d-flex align-items-center justify-content-center vh-100 vw-100">
@@ -32,7 +16,7 @@ function Dashboard() {
       ) : (
         <>
           <div className='fadeOut'>
-            Bem Vindo, <b>Usuário X</b>
+            Bem Vindo, <b>Usuário {user.nome}</b>
           </div>
           <div className="fadeIn">
             <div className="buttonContainer d-block my-3">
