65c03e0ec3b00f097d9453d3478be679436d8430
Natanael <natanael_magalhaes@msn.com>
2021-07-15 17:01:01 -0300

    usando `PrivateComponent` nas rotas que exigem autenticação

--- client/src/App.js
+++ client/src/App.js
@@ -10,29 +10,34 @@ import Home from './Home'
 import Dashboard from './Dashboard'
 import PowerBI from './PowerBI'
 import { AuthProvider } from './AuthContext'
+import PrivateComponent from './PrivateComponent'
 
 function App() {
   return (
     <AuthProvider>
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/register">
-          <Register />
-        </Route>
-        <Route path="/dashboard">
-          <Dashboard />
-        </Route>
-        <Route path="/powerbi">
-          <PowerBI />
-        </Route>
-      </Switch>
-    </Router>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/dashboard">
+            <PrivateComponent>
+              <Dashboard />
+            </PrivateComponent>
+          </Route>
+          <Route path="/powerbi">
+            <PrivateComponent>
+              <PowerBI />
+            </PrivateComponent>
+          </Route>
+        </Switch>
+      </Router>
     </AuthProvider>
   )
 }
