28f4393b129274a0c7ded032390746160ac3160f
Natanael <natanael_magalhaes@msn.com>
2021-07-17 00:49:02 -0300

    validações no formulário

--- api/routes/formulario.js
+++ api/routes/formulario.js
@@ -1,9 +1,19 @@
 const express = require('express')
+const { validationResult } = require('express-validator')
+const { formChain } = require('../middleware/validation_chains')
 const Formulario = require('../models/Formulario')
 
 const router = express.Router()
 
-router.post('/', async (req, res) => {
+router.post('/', formChain, async (req, res) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.json({
+      success: false,
+      errors
+    })
+  }
+
   const data = req.body
   const form = Formulario.build({
     id_unidade_saude: data.id_unidade_saude,
@@ -15,29 +25,33 @@ router.post('/', async (req, res) => {
     lote: data.lote,
     data_vacinacao: new Date(data.data_vacinacao),
     dose: data.dose,
-    cns: data.cns,
     nome_mae: data.nome_mae,
     nome_social: data.nome_social,
     sexo: data.sexo,
     raca: data.raca,
     gestante: data.gestante,
     puerpera: data.puerpera,
-    pais_residencia: data.pais_residencia,
     zona: data.zona,
     logradouro: data.logradouro,
     numero: data.numero,
     bairro: data.bairro,
     complemento: data.complemento,
-    email: data.email,
     cep: data.cep
   })
 
   try {
     await form.save()
+    res.json({
+      success: true,
+      message: 'Formulário cadastrado com sucesso'
+    })
   } catch (e) {
     console.log(e)
+    res.json({
+      success: false,
+      message: 'Não foi possível cadastrar o formulário'
+    })
   }
-  res.json({ success: true, message: 'Formulário cadastrado com sucesso' })
 })
 
 module.exports = router
